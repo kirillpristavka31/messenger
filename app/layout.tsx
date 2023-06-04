684b745eb062c34366d50e54cf3fdd648cccc513
kirillpristavka31 <kirillpristavka31@yandex.ru>
2023-06-04 08:22:32 +0300

    My first commit!

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import { Inter } from 'next/font/google'
+
+import ToasterContext from './context/ToasterContext'
+
+import './globals.css'
+import AuthContext from './context/AuthContext'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Messenger Clone',
+  description: 'Messenger Clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
